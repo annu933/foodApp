f150ffa90fbc1a4a0644c03c5d5c0a0d4951de96
annu <kashyapannu933@gmail.com>
2023-11-21 08:46:05 -0700

    day1:build welcome and login page

---
+++ src/screens/LoginSignuScreen/SignupScreens.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text,View,StyleSheet } from 'react-native'
+export const SignupScreens = () => {
+  return (
+    <View>
+        <Text>SignupScreens</Text>
+    </View>
+  )
+}
